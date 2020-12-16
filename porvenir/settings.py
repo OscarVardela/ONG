315950f25877f3518b12d3b708421c1df6a3c04f
Oscar Vargas Tudela <oscarvargastudela94@yahoo.com>
2020-12-16 14:47:21 -0400

    V10

--- porvenir/settings.py
+++ porvenir/settings.py
@@ -121,9 +121,9 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "fundacionporvenir.ong@gmail.com"
 EMAIL_HOST_PASSWORD = "fundacion2020"
+EMAIL_PORT = 587
